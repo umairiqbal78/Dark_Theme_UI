fe0e2f5dc9e2ad00ab5d520a6611cd72846708d7
Umair-Iqbal <51910760+umairiqbal78@users.noreply.github.com>
2021-07-17 02:20:56 +0500

    updating changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -27,7 +28,7 @@ ThemeData _lightTheme = ThemeData(
 ThemeData _darkTheme = ThemeData(
     accentColor: Colors.red,
     brightness: Brightness.dark,
-    primaryColor: Colors.amber);  
+    primaryColor: Colors.amber);
 bool _light = true;
 
 class _DarkLightThemeState extends State<DarkLightTheme> {
@@ -45,7 +46,7 @@ class _DarkLightThemeState extends State<DarkLightTheme> {
       shadowColor: Colors.transparent);
   var operStyle = TextStyle(color: Colors.green);
   var numStyle = TextStyle(color: Colors.grey);
-  
+
   var result = "";
   Widget btn(var textt, var btnstyle) {
     return ElevatedButton(
@@ -77,19 +78,17 @@ class _DarkLightThemeState extends State<DarkLightTheme> {
 
     ContextModel cm = ContextModel();
     double eval = exp.evaluate(EvaluationType.REAL, cm);
-    
 
     setState(() {
       result = eval.toString();
       expression = expression;
-
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      theme: _light ? _lightTheme: _darkTheme,
+        theme: _light ? _lightTheme : _darkTheme,
         title: 'Calculator',
         home: Scaffold(
           appBar: AppBar(
@@ -104,100 +103,80 @@ class _DarkLightThemeState extends State<DarkLightTheme> {
                   })
             ],
           ),
-          body: Container(
-      padding: EdgeInsets.all(10.0),
-      margin: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 10),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(expression,
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.grey,
+                    )),
+                Text(result,
+                    style: TextStyle(
+                      fontSize: 56,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  children: [
+                    btn("1", numStyle),
+                    btn("2", numStyle),
+                    btn("3", numStyle),
+                    btn("/", operStyle)
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Row(
+                  children: [
+                    btn("4", numStyle),
+                    btn("5", numStyle),
+                    btn("6", numStyle),
+                    btn("*", operStyle),
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Row(
+                  children: [
+                    btn("7", numStyle),
+                    btn("8", numStyle),
+                    btn("9", numStyle),
+                    btn("+", operStyle),
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                        style: style,
+                        onPressed: clearr,
+                        child: Text(
+                          "AC",
+                          style: TextStyle(color: Colors.grey),
+                        )),
+                    btn("0", numStyle),
+                    ElevatedButton(
+                        style: style,
+                        onPressed: output,
+                        child: Text(
+                          "=",
+                          style: TextStyle(color: Colors.green),
+                        )),
+                    btn("-", operStyle),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(expression,
-              style: TextStyle(
-                fontSize: 36,
-                color : Colors.grey,
-              )),
-          Text(result,
-              style: TextStyle(
-                fontSize: 56,
-                fontWeight: FontWeight.bold,
-              )),
-          SizedBox(
-            height: 100,
-          ),
-          Row(
-            children: [
-              btn("1", numStyle),
-              btn("2", numStyle),
-              btn("3", numStyle),
-              btn("/", operStyle)
-            ],
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Row(
-            children: [
-              btn("4", numStyle),
-              btn("5", numStyle),
-              btn("6", numStyle),
-              btn("*", operStyle),
-            ],
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Row(
-            children: [
-              btn("7", numStyle),
-              btn("8", numStyle),
-              btn("9", numStyle),
-              btn("+", operStyle),
-            ],
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                  style: style,
-                  onPressed: clearr,
-                  child: Text(
-                    "AC",
-                    style: TextStyle(color: Colors.grey),
-                  )),
-              btn("0", numStyle),
-              ElevatedButton(
-                  style: style,
-                  onPressed: output,
-                  child: Text(
-                    "=",
-                    style: TextStyle(color: Colors.green),
-                  )),
-              btn("-", operStyle),
-            ],
-          ),
-        ],
-      ),
-    ),
         ));
   }
 }
